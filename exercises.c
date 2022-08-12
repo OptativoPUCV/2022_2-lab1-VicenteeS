df3696795d7d74c44298e8b7e9c40ad7c44adf2c
Replit user <>
2022-08-12 12:56:45 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -41,12 +41,12 @@ void sumaNultimos(int a[], int n, int m, int *suma)
 {
   int arrayCopia[m];
   int j=0;
-  for (int i = n-1 ; i>=m ; i--)
+  for (int i = n ; i>m ; i--)
   {
     arrayCopia[j] = a[i];
     j++;
   }
-  *suma = sumaN(arrayCopia, m+1);
+  *suma = sumaN(arrayCopia, m);
 }
 
 /*
@@ -64,7 +64,7 @@ typedef struct {
 
 Persona *crearPersona(char nombre[], char rut[], int edad)
 {
-  
+  //strcpy()
   
   return NULL;
 }
