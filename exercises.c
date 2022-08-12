d0c2e59ce4e0b82308ae8bdc22f5a16b8e616ca0
Replit user <>
2022-08-12 13:12:18 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -88,7 +88,7 @@ typedef struct {
 
 Vector *crearVector(int n) 
 {
-  Vector* arreglo = (vector *) calloc (n , sizeof(Vector));
+  Vector* arreglo = (Vector *) calloc (n , sizeof(Vector));
   return NULL; 
 }
 
