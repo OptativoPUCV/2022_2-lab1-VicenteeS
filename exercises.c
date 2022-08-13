6d03f9267d430169b258fb36b75e3eaccd0bb6d1
Replit user <>
2022-08-13 04:28:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -74,9 +74,9 @@ Persona *crearPersona(char nombre[], char rut[], int edad)
   nuevaP = (Persona *)malloc(sizeof(Persona));
   //Persona *nuevaP[1];
   
-  scanf("%s", nuevaP[0]->nombre);
+  scanf("%s", nuevaP[0].nombre);
   // &nuevaP->rut, &nuevaP->edad);
-  scanf("%s", nuevaP[0]->rut);
+  scanf("%s", nuevaP[0].rut);
   scanf("%d", &nuevaP[0]->edad);
   
   return (nuevaP);
